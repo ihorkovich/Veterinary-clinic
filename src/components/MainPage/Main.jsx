604eabadd2d1975833e36a1bb7e939fa2bb9ae04
ihorkovich <igorzzz715@gmail.com>
2023-02-10 23:34:51 +0200

    created base routing

---
+++ src/components/MainPage/Main.jsx
@@ -0,0 +1,23 @@
+import NavBar from "../Everywhere/NavBar/NavBar";
+import Hero from "./Hero/Hero";
+import Doctors from "./Doctors/Doctors";
+import SomeServices from "./SomeServices/SomeServices";
+import QNA from "./QNA/QNA";
+import Location from "./Location/Location";
+import Footer from "../Everywhere/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div className="bg-bgGreen">
+      <NavBar display="fixed" />
+      <Hero />
+      <Doctors />
+      <SomeServices />
+      <QNA />
+      <Location />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
